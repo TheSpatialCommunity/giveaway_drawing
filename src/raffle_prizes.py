0a240d01c8406f7baea707a5361ab164252c7d8e
Daniel <RoboDonut@users.noreply.github.com>
2017-01-06 15:26:06 -0800

    added some stub code and API urls and channel IDs

--- src/raffle_prizes.py
+++ src/raffle_prizes.py
@@ -1,6 +1,29 @@
-import random
+import random, requests
 from slack_config import config
 
+def get_channel_members(channel, token):
+	""" return a list of users in a channel using channel info method 
+		https://slack.com/api/channels.info?token=XYZ&channel=C06QBUKCK
+		"id": "C3NFP1J84",
+            	"name": "raffle_devsummit_2017",
+		"id": "C3NBEP5CK",
+            	"name": "raffle_foss4g_2017",
+		"id": "C3P4B10F9",
+            	"name": "raffle_pycon_2017","""
+	user_ids = []
+	# return the user name and their ID
+	return user_ids
+
+def get_user_status (user, token):
+	""" check if user is active. looks like if the user is not found it is inactive.
+	i tested with user ID "T06QBQ0DV" (known inactive and i get "no such user" error)
+		https://slack.com/api/users.profile.get?token=XYZ&user=XYZ"""
+	for u in user:
+		if "is_found":
+			return True
+		else:
+			return False
+	
 def main():
 	""" 
 		TSC Raffle Prize 
